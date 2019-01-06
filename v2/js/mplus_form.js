129e6f54b2f90cb08a23de6418ac4d9d4ef24f33
Vladmir Cruz <vladcruz@gmail.com>
2019-01-05 22:08:53 -0200

    Atualizando o último status! Agora tá pegando forma!

--- v2/js/mplus_form.js
+++ v2/js/mplus_form.js
@@ -1,3 +1,31 @@
-/*
-JS do Vlad
-*/
+/* *****************************************************************************
+File Name: mplus_form.js
+Author: Vladmir Cruz
+Date: 05/01/2019
+Version: 0.1
+
+Change Control:
+===============
+Date          | Author                | Description
+--------------------------------------------------------------------------------
+05/01/2019    | Vladmir Cruz          | Creation of the file
+***************************************************************************** */
+
+/* Function that changes classes on <div> visibility */
+function display_div(div_id, div_visibility)
+{
+  if(div_visibility == 'show')
+  {
+    document.getElementById(div_id).className = 'div_visibility_show';
+  }
+  else if(div_visibility == 'hide')
+  {
+    document.getElementById(div_id).className = 'div_visibility_hide';
+  }
+}
+
+/* JQuery to Initialize Tooltip */
+$(document).ready(function()
+{
+  $('[data-toggle="tooltip"]').tooltip();
+});
