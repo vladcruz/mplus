f40c52e119d3a70c2dc024111ea72474ef8ec03c
Vladmir Cruz <vladcruz@gmail.com>
2019-01-10 17:12:51 -0200

    Atualizando....

--- v2/js/mplus_form.js
+++ v2/js/mplus_form.js
@@ -14,16 +14,95 @@ Date          | Author                | Description
 /* Function that changes classes on <div> visibility */
 function display_div(div_id, div_visibility)
 {
-  if(div_visibility == 'show')
+  if(div_visibility == "show")
   {
-    document.getElementById(div_id).className = 'div_visibility_show';
+    document.getElementById(div_id).className = "div_visibility_show";
   }
-  else if(div_visibility == 'hide')
+  else if(div_visibility == "hide")
   {
-    document.getElementById(div_id).className = 'div_visibility_hide';
+    document.getElementById(div_id).className = "div_visibility_hide";
   }
 }
 
 /* *****************************************************************************
 Validation Functions
 ***************************************************************************** */
+
+/* Validate the CPF Number */
+
+function validate_cpf(object_id)
+{
+  // Clear all error messages that may exist
+  document.getElementById(object_id).className = "form-control";
+
+  var cpf;
+  var cpf_sum;
+  var cpf_leftover;
+  var cpf_invalid_list;
+
+  // Creates a list of known CPFs that may be used
+  cpf_invalid_list = ["00000000000","11111111111","22222222222","33333333333","44444444444","55555555555","66666666666","77777777777","88888888888","99999999999"];
+
+  cpf = document.getElementById(object_id).value;
+
+  // Remove all unnecessary chars
+  cpf = cpf.replace(/\./g,"");
+  cpf = cpf.replace(/-/g,"");
+
+  if (cpf.length != 11 || (cpf_invalid_list.indexOf(cpf) != -1))
+  {
+      document.getElementById(object_id).className = "form-control valid_nok";
+      document.getElementById(object_id).innerHTML = "";
+  }
+  else
+  {
+    // Verify that the first CPF Digit is Valid
+    cpf_sum = 0;
+    for (i=1; i<=9; i++)
+    {
+      cpf_sum = cpf_sum + parseInt(cpf.substring(i-1, i)) * (11 - i);
+      cpf_leftover = (cpf_sum * 10) % 11;
+    }
+
+    if ((cpf_leftover == 10) || (cpf_leftover == 11))
+    {
+      cpf_leftover = 0;
+    }
+
+    if (cpf_leftover != parseInt(cpf.substring(9, 10)) )
+    {
+      document.getElementById(object_id).className = "form-control valid_nok";
+      document.getElementById(object_id).innerHTML = "";
+    }
+    else
+    {
+      document.getElementById(object_id).className = "form-control valid_ok";
+    }
+
+    // Verify that the second CPF Digit is Valid
+    cpf_sum = 0;
+    for (i = 1; i <= 10; i++)
+    {
+      cpf_sum = cpf_sum + parseInt(cpf.substring(i-1, i)) * (12 - i);
+      cpf_leftover = (cpf_sum * 10) % 11;
+    }
+
+    if ((cpf_leftover == 10) || (cpf_leftover == 11))
+    {
+      cpf_leftover = 0;
+    }
+
+    if (cpf_leftover != parseInt(cpf.substring(10, 11) ) )
+    {
+      document.getElementById(object_id).className = "form-control valid_nok";
+      document.getElementById(object_id).innerHTML = "";
+    }
+    else
+    {
+      document.getElementById(object_id).className = "form-control valid_ok";
+    }
+  }
+}
+
+/* Validate the Name */
+function
