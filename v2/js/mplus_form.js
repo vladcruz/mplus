b2896c832053360b8908c8167423f594ed23802b
Vladmir Cruz <vladcruz@gmail.com>
2019-01-11 11:15:09 -0200

    Atualizando o de hoje!

--- v2/js/mplus_form.js
+++ v2/js/mplus_form.js
@@ -11,7 +11,53 @@ Date          | Author                | Description
 05/01/2019    | Vladmir Cruz          | Creation of the file
 ***************************************************************************** */
 
-/* Function that changes classes on <div> visibility */
+// Global Variables
+var global_person_vars;
+var global_company_vars;
+
+// Initializing Global Variables
+// Those are used to verify if a mandatory field has been filled or not
+// also verifies that in case the field has been filled, it is correct
+// CODES: 0 - Not filled at all | 1 - Filled correctly | -1 Filled incorrectly
+global_person_vars = {
+                    "person_cpf:0",
+                    "person_fname:0",
+                    "person_lname:0",
+                    "person_email:0",
+                    "person_bdate:0",
+                    "person_mphone:0",
+                    "person_cep:0",
+                    "person_street:0",
+                    "person_street_number:0";
+                    "person_state:0",
+                    "person_city:0",
+                    "person_city_area:0",
+                    "vehicle_plate:0",
+                    "vehicle_chassis:0",
+                    "vehicle_renavam:0",
+                    "insurance_renovation:0",
+                    "insurance_company:0",
+                    "insurance_bonus:0",
+                    "insurance_accident:0"
+                    };
+
+
+global_company_vars = {
+                      "company_cnpj:0",
+                      "company_name:0",
+                      "company_cep:0",
+                      "company_street:0",
+                      "company_street_number:0",
+                      "company_state:0",
+                      "company_city:0",
+                      "company_city_area:0",
+                      "contact_fname:0",
+                      "contact_lname:0",
+                      "contact_email:0",
+                      "contact_mphone:0"
+                      };
+
+// Function that changes classes on <div> visibility
 function display_div(div_id, div_visibility)
 {
   if(div_visibility == "show")
@@ -28,7 +74,13 @@ function display_div(div_id, div_visibility)
 Validation Functions
 ***************************************************************************** */
 
-/* Validate the CPF Number */
+// Validate Central
+function validate_central()
+{
+
+}
+
+// Validate the CPF Number
 
 function validate_cpf(object_id)
 {
@@ -103,6 +155,3 @@ function validate_cpf(object_id)
     }
   }
 }
-
-/* Validate the Name */
-function
