6ad8a5bbab1eccf2e3995c568734780a78382e93
Vladmir Cruz <vladcruz@gmail.com>
2019-03-21 09:12:18 -0300

    Atualizando o dia de hoje! só pra começar!

--- v2/js/mplus_validate_person.js
+++ v2/js/mplus_validate_person.js
@@ -706,60 +706,6 @@ function enable_person_use_study(object_id_1, object_id_2)
 }
 
 /* **************************************************************************
-Validate Insurance Renewal
-************************************************************************** */
-function validate_insurance_renewal(object_id_1, object_id_2, object_id_3, object_id_4)
-{
-  var insurance_renewal;
-
-  insurance_renewal = document.getElementById(object_id_1).Value;
-
-  if(insurance_renewal == "---")
-  {
-    document.getElementById(object_id_1).className = "form-control valid_nok";
-  }
-  else
-  {
-    enable_insurance_renewal(object_id_1, object_id_2, object_id_3, object_id_4);
-    document.getElementById(object_id_1).className = "form-control valid_ok";
-  }
-}
-
-/* **************************************************************************
-Enable Insurance Renewal
-************************************************************************** */
-function enable_insurance_renewal(object_id_1, object_id_2, object_id_3, object_id_4)
-{
-  var insurance_renewal;
-  var insurance_company;
-  var insurance_bonus;
-  var insurance_accident;
-
-  insurance_renewal = document.getElementById(object_id_1).value;
-  insurance_company = document.getElementById(object_id_2);
-  insurance_bonus = document.getElementById(object_id_3);
-  insurance_accident = document.getElementById(object_id_4);
-
-  if(insurance_renewal == "---" || insurance_renewal == "no")
-  {
-    insurance_company.setAttribute("disabled", "disabled");
-    insurance_company.selectedIndex = 0;
-
-    insurance_bonus.setAttribute("disabled", "disabled");
-    insurance_bonus.selectedIndex = 0;
-
-    insurance_accident.setAttribute("disabled", "disabled");
-    insurance_accident.selectedIndex = 0;
-  }
-  else
-  {
-    insurance_company.removeAttribute("disabled");
-    insurance_bonus.removeAttribute("disabled");
-    insurance_accident.removeAttribute("disabled");
-  }
-}
-
-/* **************************************************************************
 Validate Car Plate (Format Only)
 ************************************************************************** */
 function validate_vehicle_plate(object_id)
@@ -881,5 +827,59 @@ function enable_vehicle_new(object_1, object_2, object_3)
     {
       document.getElementById(object_id_3).className = "form-control valid_ok";
     }
+  }
+}
+
+/* **************************************************************************
+Validate Insurance Renewal
+************************************************************************** */
+function validate_insurance_renewal(object_id_1, object_id_2, object_id_3, object_id_4)
+{
+  var insurance_renewal;
+
+  insurance_renewal = document.getElementById(object_id_1).Value;
+
+  if(insurance_renewal == "---")
+  {
+    document.getElementById(object_id_1).className = "form-control valid_nok";
+  }
+  else
+  {
+    enable_insurance_renewal(object_id_1, object_id_2, object_id_3, object_id_4);
+    document.getElementById(object_id_1).className = "form-control valid_ok";
+  }
+}
+
+/* **************************************************************************
+Enable Insurance Renewal
+************************************************************************** */
+function enable_insurance_renewal(object_id_1, object_id_2, object_id_3, object_id_4)
+{
+  var insurance_renewal;
+  var insurance_company;
+  var insurance_bonus;
+  var insurance_accident;
+
+  insurance_renewal = document.getElementById(object_id_1).value;
+  insurance_company = document.getElementById(object_id_2);
+  insurance_bonus = document.getElementById(object_id_3);
+  insurance_accident = document.getElementById(object_id_4);
+
+  if(insurance_renewal == "---" || insurance_renewal == "no")
+  {
+    insurance_company.setAttribute("disabled", "disabled");
+    insurance_company.selectedIndex = 0;
+
+    insurance_bonus.setAttribute("disabled", "disabled");
+    insurance_bonus.selectedIndex = 0;
+
+    insurance_accident.setAttribute("disabled", "disabled");
+    insurance_accident.selectedIndex = 0;
+  }
+  else
+  {
+    insurance_company.removeAttribute("disabled");
+    insurance_bonus.removeAttribute("disabled");
+    insurance_accident.removeAttribute("disabled");
   }
 }
